712acd8a985fe5f6f243cd49132b9f358806962a
Daniel <52731744+computerwizard1991@users.noreply.github.com>
2022-02-21 13:45:45 -0500

    Added logs for Player 1 output

--- Main.py
+++ Main.py
@@ -16,9 +16,10 @@ def main():
     g = genetic(memDepth, nodeSize, popSize, mutationRate, generations)
 
     p1 = Player(len(g[1]), g[0], g[1])
-    p2 = Player(2)  # Strategy and Initial Moves default to random
+    p2 = Player(3)  # Strategy and Initial Moves default to random
     (p1s, p2s) = playPrisonersDillema(p1, p2, 1000)
     # Strategy and Initial Moves default to random("Player 1 ID:", p1.strategy, p1.initMoves)
+    print("Player 1 ID:", p1.strategy, p2.initMoves)
     print("Player 2 ID:", p2.strategy, p2.initMoves)
     print("Player 1 score:", p1.score)
     print("Player 2 score:", p2.score)
