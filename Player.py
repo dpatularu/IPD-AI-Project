9ed1c11215b67b386c125afdcb2dbad45cd49181
Matthew <faceincake@gmail.com>
2022-02-24 17:09:42 -0500

    Updated Player with Dna

--- Player.py
+++ Player.py
@@ -1,90 +1,120 @@
 """Contains the Player class and also related functions
 
-CD Strings should follow the `encode_CD` and `decode_CD` functions.
-Take for example 'CDCC', this would encode to 2, (or 0010 in binary).
-So reverse the string, and convert C->0 and D->1, and that's your number in binary.
+Dna of a Player is strategy CONCAT initMoves
+- So 'CCDCD' would initially defect and always cooperate unless they were a sucker last round
+- So 'CCDCD' would be a strategy ID of 4 and an initMoves ID of 1 with an overall ID of 9
 """
+
+from typing import Tuple
+from Dna import Dna
+
 
 class Player:
     OUTCOMESA :list = ["R", "T", "S", "P"]
     OUTCOMESD :dict = { v:k for k,v in enumerate(OUTCOMESA) } # factors as dictionary
     NODESIZE :int = len(OUTCOMESA)
-    NODEDEPTH :int = 2 # log_2 (NODESIZE) -> int
+    NODEDEPTH :int = NODESIZE.bit_length()-1 # log_2 (NODESIZE)
 
-    def __init__ (self, memoryDepth:int, strategy:int, initialMoves:int):
-        """Creates a new player with the given memory depth, strategy, and initial moves.
-
-        You can use any of the `from_<blank>` class methods to construct a Player.
-
-        Remember to initialize the player before playing the game.
-        Use the initializePlayers function that's in this file.
-        """
-        self.memDepth  :int = memoryDepth # In bits
-        self.initMoves :int = initialMoves
-        assert self.initMoves < 2**self.memDepth, "InitialMoves > highest possible value with given MemoryDepth"
-        self.strategy  :int = strategy
-        self.stratSize :int = Player.NODESIZE ** self.memDepth
-        assert self.strategy < 2**self.stratSize, "Strategy > highest possible value with the given MemoyyDepth"
+    def __init__ (self, strategy:Dna, initialMoves:Dna):
+        """Creates a player using the given strategy and initial moves"""
+        self.initMoves :Dna = initialMoves
+        self.strategy  :Dna = strategy
+        if Player.NODESIZE**self.memDepth != self.stratSize: raise ValueError
         self.curState :int = 0 # Starts empty, needs initializing
         self.stateMask :int = self.stratSize - 1 # To keep the curState small
         self.initialized :bool = False
         self.score :int = 0 # Player wants to maximize this
+    
+    @property
+    def memDepth (self)->int:
+        """Returns the memory depth of this player"""
+        return self.initMoves.size
+    
+    @memDepth.setter
+    def memDepth (self, m:int):
+        """Sets the memory depth of this Player, fills """
+        if m <= 0 or m > 12: raise ValueError
+        self.initMoves.size = m
+        self.strategy.size = Player.NODESIZE ** m
+    
+    @property
+    def stratSize (self)->int:
+        """Returns the number of bits needed to store this Player's strategy"""
+        return self.strategy.size
+    
+    @stratSize.setter
+    def stratSize (self, s:int):
+        if s < Player.NODESIZE or s%4!=0 or s.bit_length()%2!=1: raise ValueError
+        self.strategy.size = s
+        self.initMoves.size = s.bit_length().bit_length()-1 # math.log can eat my ass
+
+    @classmethod
+    def calcStratSize (cls, memDepth:int)->int:
+        """Returns the number of bits needed to store a strategy with memory depth `memDepth`"""
+        return cls.NODESIZE**memDepth
+
+    @staticmethod
+    def __split__(d:Dna)->Tuple[Dna,Dna]:
+        s :int = 1<<(d.size.bit_length()-1)
+        S :str = str(d)
+        return (Dna(S[:s]), Dna(S[s:]))
+
+    @staticmethod
+    def __combine__(d1:Dna, d2:Dna)->Dna:
+        return Dna(str(d1)+str(d2))
 
     @classmethod
     def from_str (cls, strategy:str, initialMoves:str):
-        """Creates a Player using the given strings `strategy` and `initialMoves`
-        
-        The memory depth is interpreted from the length of `initialMoves`.
-        The length of `strategy` should be 4**memDepth.
+        """Constructs a player using the two strings `strategy` and `initialMoves`.
+
+        For example, ("DDDD", "C"), would construct a player who initially cooperates but always defects.
         """
-        s :int = encode_CD(strategy)
-        i :int = encode_CD(initialMoves)
-        return cls(len(initialMoves), s, i)
+        return cls(Dna(strategy), Dna(initialMoves))
 
     @classmethod
-    def from_dna (cls, dna:str):
-        """Creates a Player using the given string `dna`.
-        
-        `dna` can be thought of as strategy CONCAT initMoves.
-        
-        For example, 'CCCCD', is an always-cooperate player who initially defects."""
-        L :int = len(dna) # Length of DNA
-        s :int = 4 # Splitting index between strat and init. moves
-        while s<L>>1:s<<=2 # Find where s should be with respect to L
-        return cls.from_str(dna[:s], dna[s:])
+    def from_dna (cls, d:Dna):
+        """Constructs a player using the given Dna `d`.
+        The Dna object is appropriately split into a strategy and initial move(s). Check Dna for more info.
+        """
+        (d1, d2) = Player.__split__(d)
+        return cls(d1, d2)
 
     @classmethod
     def from_id (cls, memoryDepth:int, id:int):
         """Creates a Player using the given `memoryDepth` and `id`.
         
-        `id` is number where each bit represents a C or D.
+        `id` is an integer where each bit represents a C or D.
         
         For example, with a memDepth of 1, an id of 3 would be a
         strategy of 'DCCC' and initMove of 'D'."""
-        strat :int = id >> memoryDepth
-        init  :int= id & ((1 << memoryDepth) - 1)
-        return cls(memoryDepth, strat, init)
+        (d1, d2) = Player.__split__(Dna(id, cls.calcStratSize(memoryDepth)+memoryDepth))
+        return cls(d1, d2)
 
     def getMove (self)->str:
         """Returns whether this player would C or D for the current history and strategy"""
         if not self.initialized:
-            print("Error, player was not itialized yet!")
-            exit(-1)
-        return "D" if (self.strategy >> self.curState) & 1 else "C"
+            raise Exception("Player not initialized yet")
+        return "D" if self.strategy[self.curState] else "C"
         
     def updateHistory (self, outcome:str):
         """Updates the history with the result of last round"""
         self.curState <<= Player.NODEDEPTH
         self.curState = self.curState & self.stateMask | Player.OUTCOMESD[outcome]
-    
-    def strategy_str (self)->str:
-        """String representation of this player's strategy"""
-        return decode_CD(self.stratSize, self.strategy)
-    
-    def initMoves_str (self)->str:
-        """String representation of this player's initial moves"""
-        return decode_CD(self.memDepth, self.initMoves)
-    
+
+    def __str__ (self):
+        return str(self.strategy) + str(self.initMoves)
+
+    def __eq__(self, o) -> bool:
+        if isinstance(o, Player):
+            return  self.strategy==o.strategy and self.initMoves==o.initMoves and self.curState==o.curState
+        elif isinstance(o, Dna):
+            return o == str(self)
+        elif isinstance(o, str):
+            return str(self) == o
+        elif isinstance(o, int):
+            return o == (self.strategy.val << self.initMoves.size) | self.initMoves.val
+        else: raise ValueError        
+
     def curState_str (self)->str:
         """String representation of this player's current state, or memory"""
         s :int = self.curState
@@ -94,36 +124,14 @@ class Player:
             s >>= 2
         return result
 
-def decode_CD (size:int, n:int)->str:
-    """Decodes the given integer `n` into a string of Cs and Ds of size `size`"""
-    result :str = ""
-    step :int = 1
-    for m in range(size):
-        result += "D" if n & step else "C"
-        step <<= 1
-    return result
-
-def encode_CD (s:str)->int:
-    """Encodes the CD string `s` into an integer"""
-    result :int = 0
-    step :int = 1
-    RES :dict = {"C":False, "D":True}
-    for i in range(len(s)):
-        if RES[s[i]]:
-            result |= step
-        step <<= 1
-    return result
-
 def initialize_players (p1:Player, p2:Player):
     """Prepopulate the history of both given players using their initial moves"""
     p1.score = 0
     p2.score = 0
-    p1M = p1.memDepth
-    p2M = p2.memDepth
-    M = max(p1M, p2M)
+    M = max(p1.memDepth, p2.memDepth)
     for m in range(M):
-        p1m = ("D" if (p1.initMoves >> m) & 1 else "C") if m < p1M else p1.getMove()
-        p2m = ("D" if (p2.initMoves >> m) & 1 else "C") if m < p2M else p2.getMove()
+        p1m = ("D" if p1.initMoves[m] else "C") if m < p1.memDepth else p1.getMove()
+        p2m = ("D" if p2.initMoves[m] else "C") if m < p2.memDepth else p2.getMove()        
         result = p1m + p2m
         if result == "CC":
             p1.updateHistory("R")
@@ -137,8 +145,6 @@ def initialize_players (p1:Player, p2:Player):
         elif result == "DD":
             p1.updateHistory("P")
             p2.updateHistory("P")
-        else:
-            print("Error, invalid move")
-            exit(-1)
+        else: raise ValueError
     p1.initialized = True
     p2.initialized = True
--- PlayerTester.py
+++ PlayerTester.py
@@ -1,89 +1,62 @@
 
 from Player import *
 
-def test_player_from (p, m, s, i):
-    assert p.memDepth == m
-    assert p.strategy == s
-    assert p.initMoves == i
-    return p
-
 def test_everything ():
 
-    assert encode_CD("C") == 0
-    assert encode_CD("D") == 1
-    assert encode_CD("DCCC") == 1
-    assert encode_CD("CCCD") == 8
-    assert encode_CD("DDDDDDDDDDDDDDDDDD") == 262143
-
-    p :Player = Player(1, 0, 0)
-    assert p.stateMask == 3
+    p = Player(Dna("DDDD"), Dna("C"))
+    assert p.memDepth == 1
+    assert p.strategy == "DDDD"
+    assert p.initMoves == "C"
     assert p.stratSize == 4
-    assert p.strategy_str() == "CCCC"
-    assert p.initMoves_str() == "C"
-    assert p.curState_str() == "R"
-
-    test_player_from(
-        Player.from_str("DDDD", "D"),
-        1, 15, 1
-    )
-    test_player_from(
-        Player.from_str("CDCCCCCCCCCCCDCC", "CC"),
-        2, 8194, 0
-    )
-    test_player_from(
-        Player.from_dna("CCDCC"),
-        1, 4, 0
-    )
-    test_player_from(
-        Player.from_dna("CCCCCCCCCCCCCCDDDD"),
-        2, 49152, 3
-    )
-    pb = test_player_from(
-        Player.from_id(1, 3),
-        1, 1, 1
-    )
-    assert pb.strategy_str() == "DCCC"
-    assert pb.initMoves_str() == "D"
-    p2 = test_player_from(
-        Player.from_id(2, 9),
-        2, 2, 1
-    )
-    assert p2.strategy_str() == "CDCCCCCCCCCCCCCC"
-    assert p2.initMoves_str() == "DC"
-
-    initialize_players(p, pb)
-    assert p.initMoves_str() == "C"
-    assert pb.initMoves_str() == "D"
-    assert p.curState_str() == "S"
-    assert pb.curState_str() == "T"
+    assert p.curState == 0
+    assert p.stateMask == 3
     assert p.score == 0
-    assert pb.score == 0
-    
-    assert p.getMove() == "C"
-    assert pb.getMove() == "C"
-    p.updateHistory("R")
-    pb.updateHistory("R")
-    assert p.curState_str() == "R"
-    assert pb.curState_str() == "R"
-    assert p.getMove() == "C"
-    assert pb.getMove() == "D"
+    assert p.initialized is False
+    assert Player.calcStratSize(2) == 16
 
-    p2b = Player(2, 1, 1)
-    initialize_players(p2, p2b)
+    p = Player.from_str("CCDC", "C")
+    p2 = Player.from_dna(Dna("CCDCC"))
+    assert p == 8
+    assert p == Dna("CCDCC")
+    assert p == "CCDCC"
+    assert p2 == 8
+    assert p2 == "CCDCC"
+    assert p == p2
+    p = Player.from_dna(Dna.from_all_defect(5))
+    assert p == "DDDDD"
+    p = Player.from_id(1, 2)
+    assert p == "CDCCC"
+    del p
+
+    p1 = Player.from_str("CCCC", "D")
+    p2 = Player.from_str("DDDD", "C")
+
+    initialize_players(p1, p2)
+    assert p1.initMoves == "D"
+    assert p2.initMoves == "C"
+    assert p1.curState == 1
+    assert p1.curState_str() == "T"
+    assert p2.curState == 2
+    assert p2.curState_str() == "S"
+    assert p1.score == 0
     assert p2.score == 0
-    assert p2b.score == 0
-    assert p2.initMoves_str() == "DC"
-    assert p2b.initMoves_str() == "DC"
-    assert p2.curState_str() == "RP"
-    assert p2b.curState_str() == "RP"
-    assert p2.getMove() == "C"
-    assert p2b.getMove() == "C"
-    p2.updateHistory("R")
-    p2b.updateHistory("R")
-    assert p2.curState_str() == "RR"
-    assert p2b.curState_str() == "RR"
-    assert p2.getMove() == "C"
-    assert p2b.getMove() == "D"
+    assert p1.getMove() == "C"
+    assert p2.getMove() == "D"
+    p1.updateHistory("S")
+    p2.updateHistory("T")
+    assert p1.curState_str() == "S"
+    assert p2.curState_str() == "T"
+    assert p1.getMove() == "C"
+    assert p2.getMove() == "D"
+    p1.score += 30
+
+    p3 = Player.from_str(Dna.from_all_defect(16), "CC")
+    initialize_players(p1, p3)
+    assert p1.score == 0
+    assert p1.curState_str() == "R"
+    assert p3.curState_str() == "RS"
+    assert p1.getMove() == "C"
+    assert p3.getMove() == "D"
 
     print("All tests SUCCEEDED!")
     return    
