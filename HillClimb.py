370cf5df37f86580159010280d3fef84ef26afa6
GodlewsO <oscargodlewski@gmail.com>
2022-03-03 18:45:34 -0500

    added heuristic and round parameter

--- HillClimb.py
+++ HillClimb.py
@@ -1,35 +1,36 @@
-
 from typing import Any
-from PDGame import battleRoyale
+from PDGame import *
 from Player import Player
 from SearchAlgorithms import getNeighbors
-from Generators import GenDna
+from Generators import *
 
 
-def hillClimb(memDepth: int, maxRounds:int) -> Any:
-    """note two different ways to implement comparisons
-    either compare against previous iteration's hiscore, or this iteration's highest score
-    comparing against the current iterations high score gives a more meaningful result,
-    but results in occasional infinite loops TODO
-    """
-    print("----Hill Climbing----")
+def hillClimb(memDepth: int, maxRounds: int, heuristic: str, rounds: int) -> Any:
+    """TODO"""
+
+    if heuristic == "HP":
+        opponents = GenDna.allHandpicked()
+    elif heuristic == "AMD1":
+        opponents = GenDna.allFromSize(1)
+    else:
+        print("invalid heuristic")
+        exit(1)
 
     topStrat = GenDna.random(memDepth)
     topStratScore = 0
 
     for i in range(maxRounds):
-        print("\ti:", i, " | score:", topStratScore)
 
         # generate successors for our highest performing strategy
         successors = getNeighbors(topStrat)
         successors.append(topStrat)
 
         # calculate the fitness value for each successor and the top strategy
-        scoreLst = battleRoyale(successors)
-        topStratScore = scoreLst[-1]    # compare against topStrat's score from this iteration
+        scoreLst = manyVersusMany(successors, opponents, rounds)
+        topStratScore = scoreLst[-1]  # compare against topStrat's score from this iteration
         scoreLst, stratLst = zip(*sorted(zip(scoreLst, successors)))
 
-        print(scoreLst)
+        # print(scoreLst)
 
         # compare the fitness of the recently calculated top strategy against the previous one
         if scoreLst[-1] <= topStratScore:
