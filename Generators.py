e716b8899cfc5dc69671b1321c0d1af3278b043b
GodlewsO <oscargodlewski@gmail.com>
2022-03-06 18:25:55 -0500

    updated documentation

--- Generators.py
+++ Generators.py
@@ -1,31 +1,30 @@
 import random
-from typing import Iterator, List, Tuple
+from typing import Iterator, List
 from Dna import Dna
 from Player import Player
-from SearchAlgorithms import coalesce
 
 
 class GenPlayer:
 
     @staticmethod
     def random(memoryDepth: int):
-        """Creates a random Player using `memoryDepth`"""
+        """ Creates a random Player using `memoryDepth`"""
         return Player.from_dna(GenDna.random(memoryDepth))
 
     @staticmethod
     def tit4Tat(memDepth: int) -> Player:
-        """Creates a tit-for-tat player with the given memory depth"""
+        """ Creates a tit-for-tat player with the given memory depth"""
         return Player.from_str("CCDD" * (4 ** (memDepth - 1)), "C" * memDepth)
 
     @staticmethod
     def susTit4Tat(memDepth: int) -> Player:
-        """Creates a tit-for-tat player that initially defects"""
+        """ Creates a tit-for-tat player that initially defects"""
         n: int = Player.calcStratSize(memDepth - 1)
         return Player.from_str("CCDD" * n, "D" * memDepth)
 
     @staticmethod
     def mTit4Tat(memDepth: int) -> Player:
-        """Creates a player that tats if the opponents tits `memDepth` times"""
+        """ Creates a player that tats if the opponents tits `memDepth` times"""
         s: str = "CCDD"
         for i in range(memDepth - 1):
             s = "C" * (len(s) * 2) + s + s
@@ -33,45 +32,46 @@ class GenPlayer:
 
     @staticmethod
     def grudger(memDepth: int) -> Player:
-        """Creates a player that initially cooperates but will always defect if the opponent defects once"""
+        """ Creates a player that initially cooperates, but will always
+            defect if the opponent defects once. """
         n: int = Player.calcStratSize(memDepth) - 1
         return Player.from_str("C" + "D" * n, "C" * memDepth)
 
     @staticmethod
     def all_cooperate(memDepth: int) -> Player:
-        """Creates a player that always cooperates"""
+        """ Creates a player that always cooperates. """
         n: int = Player.calcStratSize(memDepth)
         return Player.from_str("C" * n, "C" * memDepth)
 
     @staticmethod
     def all_defect(memDepth: int) -> Player:
-        """Creates a player that always defects"""
+        """ Creates a player that always defects. """
         n: int = Player.calcStratSize(memDepth)
         return Player.from_str("D" * n, "D" * memDepth)
 
     @staticmethod
     def random_list(memDepth: int, n: int) -> List[Player]:
-        """Returns `n` random strategies with the given memory depth and node size"""
-        return [GenPlayer.random(memDepth) for i in range(n)]
+        """ Returns `n` random strategies with the given memory depth and node size. """
+        return [GenPlayer.random(memDepth) for _ in range(n)]
 
     @staticmethod
     def get_random(memDepth: int, n: int) -> Iterator[Player]:
-        """Returns a generator for a list of size `n` of random players of the given `memDepth`"""
-        return (GenPlayer.random(memDepth) for i in range(n))
+        """ Returns a generator for a list of size `n` of random players of the given `memDepth`. """
+        return (GenPlayer.random(memDepth) for _ in range(n))
 
     @staticmethod
     def all_memDepth1() -> List[Player]:
-        """Returns a list of all players of memDepth 1"""
+        """ Returns a list of all players of memDepth 1. """
         return [Player.from_id(1, i) for i in range(32)]
 
     @staticmethod
     def get_memDepth1() -> Iterator[Player]:
-        """Returns a generator for all players of memDepth 1"""
+        """ Returns a generator for all players of memDepth 1. """
         return (Player.from_id(1, i) for i in range(32))
 
     @staticmethod
     def all_handpicked(memDepth: int) -> List[Player]:
-        """Returns a list of all handpicked strats"""
+        """Returns a list of all handpicked strategies. """
         return [
             GenPlayer.tit4Tat(memDepth),
             GenPlayer.susTit4Tat(memDepth),
@@ -89,19 +89,19 @@ class GenDna:
 
     @staticmethod
     def random(memDepth: int, size: int = None) -> Dna:
-        """Returns a random DNA of `size` or `memDepth`"""
+        """ Returns a random DNA of `size` or `memDepth`"""
         N: int = GenDna.__N(memDepth, size)
         return Dna(random.randint(0, (1 << N) - 1), N)
 
     @staticmethod
     def randomLst(n: int, memDepth: int, size: int = None) -> List[Dna]:
-        """Returns a list of random DNAs of `size` or `memDepth`"""
-        return [GenDna.random(memDepth, size) for i in range(n)]
+        """ Returns a list of random DNAs of `size` or `memDepth`"""
+        return [GenDna.random(memDepth, size) for _ in range(n)]
 
     @staticmethod
     def genRandom(n: int, memDepth: int, size: int = None):
-        """Returns a generator for random DNAs of `size` or `memDepth`"""
-        return (GenDna.random(memDepth, size) for i in range(n))
+        """ Returns a generator for random DNAs of `size` or `memDepth`"""
+        return (GenDna.random(memDepth, size) for _ in range(n))
 
     @staticmethod
     def allCoop() -> Dna:
@@ -137,25 +137,29 @@ class GenDna:
         """ Returns grudger strategy.
             Cooperates until opponent defects, then only defects. """
         return Dna("CDDDC")
-    
+
+    @staticmethod
     def pavlov() -> Dna:
-        """ Cooperates if it and its opponent moved alike in previous move and defects if they moved differently. Starts cooperating"""
+        """ Cooperates if it and its opponent moved alike in previous move
+            and defects if they moved differently. Cooperates on first move. """
         return Dna("CDDCC")
 
+    @staticmethod
     def susPavlov() -> Dna:
-        """ Cooperates if it and its opponent moved alike in previous move and defects if they moved differently. Starts defecting"""
+        """ Cooperates if it and its opponent moved alike in previous move
+            and defects if they moved differently. Defects on first move. """
         return Dna("CDDCD")
 
     @staticmethod
     def allFromSize(memDepth: int, size: int = None) -> List[Dna]:
-        """Returns a list of all possible DNAs with the given `size`.
-        Warning, grows exponentially."""
+        """ Returns a list of all possible DNAs with the given `size`.
+            Warning, grows exponentially. """
         N: int = GenDna.__N(memDepth, size)
         return [Dna(i, N) for i in range(2 ** N)]
 
     @staticmethod
     def allHandpicked() -> List[Dna]:
-        """Returns a list of all handpicked strats"""
+        """ Returns a list of all handpicked strategies. """
         return [
             GenDna.tit4Tat(),
             GenDna.susTit4Tat(),
